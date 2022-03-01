053516078b78722c6dd3af3d45e2e57632f35e3c
abdullahelwalid <abdallah.elmansoor@hotmail.com>
2022-03-01 14:24:50 +0800

    added new features to notes

--- app/views/note.py
+++ app/views/note.py
@@ -1,4 +1,5 @@
 from flask import request,jsonify,abort
+from datetime import datetime
 from app.views import bp
 from app import db
 from app.models.note import Note
@@ -62,5 +63,55 @@ def edit_note():
         abort(404, "note or user doesn't exist")
     
     _note.note = _new_note
+    _note.date_modified = datetime.now()
     db.session.commit()
-    return jsonify(_note.json())
+    return jsonify(_note.json()), 201
+
+@bp.route('/note', methods=['DELETE'])
+def delete_note():
+    if not request.json:
+        abort(400, "Response should be in JSON")
+    _data = request.json
+    def _validate(request) -> bool:
+        if "note_id" not in request:
+            return False
+        if "user_id" not in request:
+            return False
+        return True
+    if not _validate(_data):
+        abort(400, "one or more data field is missing")
+
+    _user_id = _data['user_id']
+    _note_id = _data['note_id']
+
+    _user = User.query.filter_by(user_id = _user_id).first()
+    _note = Note.query.filter_by(note_id = _note_id).first()
+
+    if not _user or not _note:
+        abort(404, "note or user doesn't exist")
+    
+    db.session.delete(_note)
+    db.session.commit()
+
+    return jsonify("not deleted"), 201
+
+@bp.route('/note')
+def get_user_notes():
+    if not request.json:
+        abort(400, "Response should be in JSON")
+    _data = request.json
+    def _validate(request) -> bool:
+        if "user_id" not in request:
+            return False
+        return True
+    if not _validate(_data):
+        abort(400, "one or more data field is required")
+    _user_id = _data['user_id']
+    _user = User.query.filter_by(user_id = _user_id).first()
+    if not _user:
+        abort(404, "user doesn't exist")
+
+    _notes = Note.query.filter_by(user_id = _user_id).all()
+    return jsonify({
+        "notes": [note.json() for note in _notes]
+        })
